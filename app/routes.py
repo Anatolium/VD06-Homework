2c192e0f392b225c8612f99f757ff9ec490d590c
Анатолий <lan2002@yandex.ru>
2024-06-19 04:50:52 +0300

    (update) __init__.py, routes.py, user_data_form.html

--- app/routes.py
+++ app/routes.py
@@ -2,24 +2,22 @@ from flask import render_template, request, redirect, url_for
 
 from app import app
 
-# этот список будем заполнять словарями
 posts = []
 
 @app.route("/", methods=["GET", "POST"])
 def index():
-    # используем метод POST, так как информация будет отправляться; request.method сравнивает данные с HTTP-запросом
     if request.method == 'POST':
-        # функция request.form извлекает значение из соответствующих полей
-        title = request.form.get('title')
-        content = request.form.get('content')
-        # создаём условие для проверки наличия данных в полях title и content
-        if title and content:
-            posts.append({'title': title, 'content': content})
-            # используем для обновления страницы и предотвращения повторной отправки формы; index – имя этой функции;
-            # перенаправляем пользователя на url, связанный с функцией index, то есть снова заходим на страницу,
-            # на которой были
+        # функция request.form.get извлекает значение из соответствующих полей
+        user = request.form.get('user')
+        age = request.form.get('age')
+        city = request.form.get('city')
+        hobby = request.form.get('hobby')
+        if user and age and city:
+            posts.append({'user': user, 'age': age, 'city': city, 'hobby': hobby})
+            # перезаходим на страницу: обновляем её и предотвращаем повторную отправку формы
+            # (перенаправляем пользователя на url, связанный с данной функцией index)
             return redirect(url_for('index'))
 
-    # возвращаем отрендеренный шаблон с переданными данными постов
-    # posts будет подставляться в строку {% for post in posts %} в user_data_form.html
+    # возвращаем шаблон с переданными данными постов;
+    # список posts будет подставляться в строку {% for post in posts %} в user_data_form.html
     return render_template('user_data_form.html', posts=posts)
